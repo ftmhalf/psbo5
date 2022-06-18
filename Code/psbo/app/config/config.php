64e4611cf857f274089edb5055f96aaaff2dbd19
Faisalia <78713917+Faisalia@users.noreply.github.com>
2022-06-18 23:59:09 +0700

    Add files via upload

---
+++ Code/psbo/app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('BASEURL', 'https://iaicp.xyz/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'iaih7936_flaisarly');
+define('DB_PASS', 'Faisalia2111');
+define('DB_NAME', 'iaih7936_psbo');
+
+?>
